7d8827fecf8e40e85d38ce19c76e81be608a6ae1
Arpit Anand <arpitssanand@gmail.com>
2022-06-27 00:05:27 +0530

    add readme.md

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-// const { generateRandomData } = require("./helperFunction");
+const { generateRandomData } = require("./helperFunction");
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to faker api");
+});
 
 app.get("/data", (req, res) => {
   res.send(generateRandomData());
